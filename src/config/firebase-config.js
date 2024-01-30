bb0e86471b3fa09c278908b24ed6fc58ec646204
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-01-30 14:24:03 +0100

    dodavanje firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCWMvHzn49SU4RtJs0uU1iSHsvwIcmsgM",
+  authDomain: "exer2-b0b46.firebaseapp.com",
+  projectId: "exer2-b0b46",
+  storageBucket: "exer2-b0b46.appspot.com",
+  messagingSenderId: "285650614697",
+  appId: "1:285650614697:web:ebe441716493b131876cb8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
